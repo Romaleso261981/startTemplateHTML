bff23f1d2e673ff4f5bcf0127686796f538c4ecd
lesoRoman <ladiginscormag@gmail.com>
2023-08-15 11:22:12 +0300

    finished swiper

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-
 const swiper = new Swiper('.materials__slider', {
-	loop: true,
- 
-	// If we need pagination
-	pagination: {
-	  el: '.swiper-pagination',
-	}
- });
+   loop: true,
+   autoplay: {
+      delay: 5000,
+   },
+   pagination: {
+      el: '.swiper-pagination',
+		clickable: true,
+   },
+});
